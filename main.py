7216c7ec1b916f82ce29d28660e9989955294b43
Kirill Rudenko <cs1@list.ru>
2022-09-08 18:15:47 +0300

    feat: получил адрес для загрузки комикса в vk группу

--- main.py
+++ main.py
@@ -26,11 +26,21 @@ def test_vk_api(vk_client_id: int, vk_access_token: str, vk_group_id: int, vk_al
         "access_token": vk_access_token,
         "v": api_version
     }
-
     response = requests.get(f"https://api.vk.com/method/groups.get", params=params)
     response.raise_for_status()
     vk_groups = response.json()
     pprint(vk_groups)
+
+    params = {
+        "access_token": vk_access_token,
+        "v": api_version,
+        "group_id": vk_group_id,
+        "album_id": vk_album_id
+    }    
+    response = requests.get(f"https://api.vk.com/method/photos.getUploadServer", params=params)
+    response.raise_for_status()
+    upload_server_info = response.json()
+    pprint(upload_server_info)
 
 
 def main():
@@ -51,3 +61,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+# https://api.vk.com/method/groups.get?access_token=vk1.a.3S3uN0EzsNvG7-JpCEcmfxsSg85jNHnTmxIxFxaBgpUAZHodyKU4tL9uC9zJfe3dPeTdH6B3eysBoSyeuh4KXUZ9KLUaGvB7alVMtHKABU_ryQzjM650QTYKTpX5ISMD2lJ2XZe1MPMavm7LEdR05FBvN3HDGHXbXYCh3xLjUNfh1uJBjPP1HV5R9g1nNAT7&v=5.131
+
+# {"response":{"count":33,"items":[90038880,33393308,53895352,94385733,61281696,168874636,16623569,63731512,36284347,58267631,166523849,41452622,14448489,45135634,140444023,10042064,117483992,29559271,59767730,45446503,4489985,34867792,54046061,43948962,41890491,75313533,47535457,44743077,64834463,13074721,2190892,15798779,215837914]}}
+
+
+# https://api.vk.com/method/photos.getUploadServer?access_token=vk1.a.3S3uN0EzsNvG7-JpCEcmfxsSg85jNHnTmxIxFxaBgpUAZHodyKU4tL9uC9zJfe3dPeTdH6B3eysBoSyeuh4KXUZ9KLUaGvB7alVMtHKABU_ryQzjM650QTYKTpX5ISMD2lJ2XZe1MPMavm7LEdR05FBvN3HDGHXbXYCh3xLjUNfh1uJBjPP1HV5R9g1nNAT7&v=5.131&group_id=215837914&album_id=284992581
+
+# {"response":{"album_id":284992581,"upload_url":"https:\/\/pu.vk.com\/c856312\/ss2152\/upload.php?act=do_add&mid=202050440&aid=284992581&gid=215837914&hash=9a5a7ac3681cab21e2ac1a048279700d&rhash=890c893440a19b4a7a803a0fcb286c78&swfupload=1&api=1","user_id":202050440}}
