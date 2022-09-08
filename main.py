3b83ad413ce07dfae7956c3d82185d1dce576cfa
Kirill Rudenko <cs1@list.ru>
2022-09-08 18:10:14 +0300

    feat: прочитал переменные из .env и протестировал работу vk api
    - считал переменные окружение.
    - получил список групп из личного кабинета vk.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from pprint import pprint
 import requests
 from environs import Env
 from functions import download_comics
@@ -19,11 +20,31 @@ def fetch_comics_and_metadata() -> None:
     download_comics(comics_url, comics_folder_path, f"{comics_name_template}{comics_num}.png")
 
 
+def test_vk_api(vk_client_id: int, vk_access_token: str, vk_group_id: int, vk_album_id: int) -> None:
+    api_version = 5.131
+    params = {
+        "access_token": vk_access_token,
+        "v": api_version
+    }
+
+    response = requests.get(f"https://api.vk.com/method/groups.get", params=params)
+    response.raise_for_status()
+    vk_groups = response.json()
+    pprint(vk_groups)
+
+
 def main():
     env = Env()
     env.read_env()
+    
+    vk_client_id = env.int("VK_CLIENT_ID")
+    vk_access_token = env("VK_ACCESS_TOKEN")
+    vk_group_id = env.int("VK_GROUP_ID")
+    vk_album_id = env.int("VK_ALBUM_ID")
+    
     try:
-        fetch_comics_and_metadata()
+        # fetch_comics_and_metadata()
+        test_vk_api(vk_client_id, vk_access_token, vk_group_id, vk_album_id)
     except requests.exceptions.RequestException as error:
         print('Request error:\n', error.response)
         print('Request error text:\n', error.response.text)    
