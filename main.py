4cb492cdbd5fd9dffc1c659036591bff12b0e45a
Kirill Rudenko <cs1@list.ru>
2022-09-10 18:46:05 +0300

    refactor: вынес в функции получение адреса сервера и загрузку комикса
    - publish_comics_to_vk() - функция публикует комикс на стену группы в ВК
    - get_vk_wall_upload_server_url() - получает адрес сервера вк для загрузки фото
    - upload_photo_to_vk_server() - загружает фото на сервер

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from pprint import pprint
 import requests
 from environs import Env
@@ -6,7 +7,7 @@ from functions import download_comics
 
 def fetch_comics_and_metadata() -> None:
     comics_folder_path = "files"
-    comics_name_template = "xkcd_"
+    comics_name_template = "xkcd"
     comics_num = 353
     
     response = requests.get(f"https://xkcd.com/{comics_num}/info.0.json")
@@ -15,58 +16,50 @@ def fetch_comics_and_metadata() -> None:
 
     comics_alt = comics_with_metadata["alt"]
     comics_url = comics_with_metadata["img"]
-    download_comics(comics_url, comics_folder_path, f"{comics_name_template}{comics_num}.png")
+    download_comics(comics_url, comics_folder_path, f"{comics_name_template}.png")
 
 
-def test_vk_api(vk_client_id: int, vk_access_token: str, vk_group_id: int, vk_album_id: int) -> None:
-    api_version = 5.131
+def get_vk_wall_upload_server_url(vk_access_token: str, vk_group_id: int, vk_api_version: str) -> str:
     params = {
         "access_token": vk_access_token,
-        "v": api_version,
         "group_id": vk_group_id,
-        "album_id": vk_album_id
+        "v": vk_api_version,
     }
     response = requests.get(f"https://api.vk.com/method/photos.getWallUploadServer", params=params)
     response.raise_for_status()
-    vk_upload_server_info = response.json()
-    pprint(vk_upload_server_info)
+    return response.json()["response"]["upload_url"]
 
-    with open("files/xkcd_353.png", "rb") as file:
+
+def upload_photo_to_vk_server(vk_upload_server_url: str) -> dict:
+    file = Path("files", "xkcd_354.png")
+    with open(file, "rb") as file:
         files = {
             "photo": file
         }
-        response = requests.post(vk_upload_server_info["response"]["upload_url"], files=files)
+        response = requests.post(vk_upload_server_url, files=files)
         response.raise_for_status()
-    vk_comics_upload_info = response.json()
-    pprint(vk_comics_upload_info)
+    return response.json()
+
+
+def publish_comics_to_vk(vk_access_token: str, vk_group_id: int) -> None:
+    vk_api_version = "5.131"
+    vk_upload_server_url = get_vk_wall_upload_server_url(vk_access_token, vk_group_id, vk_api_version)
+    vk_photo_upload_info = upload_photo_to_vk_server(vk_upload_server_url)
 
 
 def main():
     env = Env()
     env.read_env()
     
-    vk_client_id = env.int("VK_CLIENT_ID")
     vk_access_token = env("VK_ACCESS_TOKEN")
     vk_group_id = env.int("VK_GROUP_ID")
-    vk_album_id = env.int("VK_ALBUM_ID")
     
     try:
-        # fetch_comics_and_metadata()
-        test_vk_api(vk_client_id, vk_access_token, vk_group_id, vk_album_id)
+        # fetch_comics_from_xkcd()
+        publish_comics_to_vk(vk_access_token, vk_group_id)
     except requests.exceptions.RequestException as error:
         print('Request error:\n', error.response)
         print('Request error text:\n', error.response.text)    
 
 if __name__ == '__main__':
     main()
-
-
-
-# https://api.vk.com/method/groups.get?access_token=vk1.a.3S3uN0EzsNvG7-JpCEcmfxsSg85jNHnTmxIxFxaBgpUAZHodyKU4tL9uC9zJfe3dPeTdH6B3eysBoSyeuh4KXUZ9KLUaGvB7alVMtHKABU_ryQzjM650QTYKTpX5ISMD2lJ2XZe1MPMavm7LEdR05FBvN3HDGHXbXYCh3xLjUNfh1uJBjPP1HV5R9g1nNAT7&v=5.131
-
-# {"response":{"count":33,"items":[90038880,33393308,53895352,94385733,61281696,168874636,16623569,63731512,36284347,58267631,166523849,41452622,14448489,45135634,140444023,10042064,117483992,29559271,59767730,45446503,4489985,34867792,54046061,43948962,41890491,75313533,47535457,44743077,64834463,13074721,2190892,15798779,215837914]}}
-
-
-# https://api.vk.com/method/photos.getUploadServer?access_token=vk1.a.3S3uN0EzsNvG7-JpCEcmfxsSg85jNHnTmxIxFxaBgpUAZHodyKU4tL9uC9zJfe3dPeTdH6B3eysBoSyeuh4KXUZ9KLUaGvB7alVMtHKABU_ryQzjM650QTYKTpX5ISMD2lJ2XZe1MPMavm7LEdR05FBvN3HDGHXbXYCh3xLjUNfh1uJBjPP1HV5R9g1nNAT7&v=5.131&group_id=215837914&album_id=284992581
-
-# {"response":{"album_id":284992581,"upload_url":"https:\/\/pu.vk.com\/c856312\/ss2152\/upload.php?act=do_add&mid=202050440&aid=284992581&gid=215837914&hash=9a5a7ac3681cab21e2ac1a048279700d&rhash=890c893440a19b4a7a803a0fcb286c78&swfupload=1&api=1","user_id":202050440}}
