2480a5aeba4b120e58c4fca266afd218931760c0
Kirill Rudenko <cs1@list.ru>
2022-09-10 19:40:28 +0300

    feat: публикация комикса на стене группы в ВК

--- functions.py
+++ functions.py
@@ -4,11 +4,12 @@ import requests
 import urllib.parse
 
 
-def download_comics(comics_url: str, file_path: str, filename: str) -> None:
+def download_comics(comics_url: str, file_path: str, filename: str) -> Path:
     Path(file_path).mkdir(parents=True, exist_ok=True)
     response = requests.get(comics_url)
     response.raise_for_status()
     
     file = Path(file_path, filename)
-    with open(file, "wb") as file:
-        file.write(response.content)
+    with open(file, "wb") as img:
+        img.write(response.content)
+    return file
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from environs import Env
 from functions import download_comics
 
 
-def fetch_comics_and_metadata() -> None:
+def get_comics_from_xkcd() -> tuple([Path, str]):
     comics_folder_path = "files"
     comics_name_template = "xkcd"
     comics_num = 353
@@ -14,10 +14,10 @@ def fetch_comics_and_metadata() -> None:
     response.raise_for_status()
     comics_with_metadata = response.json()
 
-    comics_alt = comics_with_metadata["alt"]
     comics_url = comics_with_metadata["img"]
-    download_comics(comics_url, comics_folder_path, f"{comics_name_template}.png")
-
+    comics_alt = comics_with_metadata["alt"]
+    comics_file = download_comics(comics_url, comics_folder_path, f"{comics_name_template}.png")
+    return comics_file, comics_alt
 
 def get_vk_wall_upload_server_url(vk_access_token: str, vk_group_id: int, vk_api_version: str) -> str:
     params = {
@@ -30,9 +30,8 @@ def get_vk_wall_upload_server_url(vk_access_token: str, vk_group_id: int, vk_api
     return response.json()["response"]["upload_url"]
 
 
-def upload_photo_to_vk_server(vk_upload_server_url: str) -> dict:
-    file = Path("files", "xkcd_354.png")
-    with open(file, "rb") as file:
+def upload_photo_to_vk_server(vk_upload_server_url: str, comics_file: Path) -> dict:
+    with open(comics_file, "rb") as file:
         files = {
             "photo": file
         }
@@ -55,11 +54,25 @@ def save_photo_in_vk_group_album(vk_access_token: str, vk_group_id: int, vk_api_
     return response.json()
 
 
-def publish_comics_to_vk(vk_access_token: str, vk_group_id: int) -> None:
+def post_photo_on_vk_group_wall(vk_access_token: str, vk_group_id: int, vk_api_version: str, vk_photo_saving_info: dict, comics_description: str) -> None:
+    params = {
+        "access_token": vk_access_token,
+        "owner_id": -vk_group_id,
+        "from_group": 1,
+        "attachments": f'photo{vk_photo_saving_info["response"][0]["owner_id"]}_{vk_photo_saving_info["response"][0]["id"]}',
+        "message": comics_description,
+        "v": vk_api_version,
+    }
+    response = requests.post("https://api.vk.com/method/wall.post", params=params)
+    response.raise_for_status()
+
+
+def publish_comics_to_vk(vk_access_token: str, vk_group_id: int, comics_file: Path, comics_description: str) -> None:
     vk_api_version = "5.131"
     vk_upload_server_url = get_vk_wall_upload_server_url(vk_access_token, vk_group_id, vk_api_version)
-    vk_photo_upload_info = upload_photo_to_vk_server(vk_upload_server_url)
+    vk_photo_upload_info = upload_photo_to_vk_server(vk_upload_server_url, comics_file)
     vk_photo_saving_info = save_photo_in_vk_group_album(vk_access_token, vk_group_id, vk_api_version, vk_photo_upload_info)
+    post_photo_on_vk_group_wall(vk_access_token, vk_group_id, vk_api_version, vk_photo_saving_info, comics_description)
 
 
 def main():
@@ -68,10 +81,13 @@ def main():
     
     vk_access_token = env("VK_ACCESS_TOKEN")
     vk_group_id = env.int("VK_GROUP_ID")
+
+    comics_folder_path = "files"
+    comics_name_template = "xkcd.png"
     
     try:
-        # fetch_comics_from_xkcd()
-        publish_comics_to_vk(vk_access_token, vk_group_id)
+        comics_file, comics_description = get_comics_from_xkcd()
+        publish_comics_to_vk(vk_access_token, vk_group_id, comics_file, comics_description)
     except requests.exceptions.RequestException as error:
         print('Request error:\n', error.response)
         print('Request error text:\n', error.response.text)    
